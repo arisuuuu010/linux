34b868e757b4dab36e0117b343636cbfbf17bfbe
arisuuuu010 <aryumbillopa@uide.edu.ec>
2025-12-04 13:19:13 +0000

    Holabandito

---
+++ script2.sh
@@ -0,0 +1,8 @@
+#Muestra información completa sobre el sistema operativo.
+uname -a
+# Muestra información detallada sobre la CPU (procesador) y la arquitectura del sistema.
+lscpu
+# Muestra la información de la memoria RAM y la memoria swap en un formato fácil de leer (usando unidades como MB o GB).
+free -h
+# Muestra el uso del espacio en disco por los sistemas de archivos montados (particiones, discos duros, etc.).
+df -h
