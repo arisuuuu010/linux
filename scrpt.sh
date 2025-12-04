05c62c057e2df25dc8fb354f89d78761104cb5e5
arisuuuu010 <aryumbillopa@uide.edu.ec>
2025-12-04 12:55:36 +0000

    mycommit

---
+++ scrpt.sh
@@ -0,0 +1,13 @@
+#Crear lista de archivos
+echo "Hola linux" > archivo.txt
+#echo nos ayuda a imprimir el texto de hola linux al momento de salir
+#> este simbolo nos ayuda a redireccionar la salida
+cat archivo.txt
+#Cat nos ayuda a tener toda la informacion que tiene el archivo por dentro
+cp archivo.txt copia.txt
+# Es la abreviatura de copy. Se utiliza para copiar archivos y directorios.
+ls -l
+#ls es la abreviatura de list. Se utiliza para listar los archivos y directorios en el directorio actual.
+#-l es una opción (o flag) que significa long format (formato largo).
+rm archivo.txt
+#rm es remover. Se utiliza para borrar archivos y directorios.
